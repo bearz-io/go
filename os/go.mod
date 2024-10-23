908bad5b1cd5a7a44a3fd290d8612eb2fa9e1de4
BowTiedBearz <179951934+bowtiedbearz@users.noreply.github.com>
2024-10-23 00:08:30 -0500

    work: implement os/ps module

--- os/go.mod
+++ os/go.mod
@@ -1,3 +1,5 @@
 module github.com/bearz-io/go/os
 
 go 1.23.1
+
+require golang.org/x/sys v0.26.0 // indirect
---
+++ os/windows.go
@@ -0,0 +1,20 @@
+//go:build windows
+
+package os
+
+const (
+	FAMILY   = "windows"
+	POSIX    = false
+	EOL      = "\r\n"
+	PATH_SEP = ";"
+	DIR_SEP  = "\\"
+	DEV_NULL = "NUL"
+)
+
+func IsWsl() bool {
+	return false
+}
+
+func IsWindows() bool {
+	return true
+}
